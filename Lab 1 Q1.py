dc6beeb7de24111f065caa1680a2f1e3860a4095
Aman-Chandresa <aman.chandresa2106@gmail.com>
2025-04-27 21:44:53 +0530

    Update and rename README.md to Lab 1 Q1.py

---
+++ Lab 1 Q1.py
@@ -0,0 +1,6 @@
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+sum=num1+num2
+print("The sum is: ",sum)
+print("Name: Aman Chandresa")
+print("Roll Number: 24BCL003")
